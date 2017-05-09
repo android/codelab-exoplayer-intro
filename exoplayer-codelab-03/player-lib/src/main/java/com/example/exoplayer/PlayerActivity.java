e6a3697dfd9eb9f6987b46c7ad148173ac29486b
Sam Thorogood <sam.thorogood@gmail.com>
2017-05-09 15:55:21 -0700

    Initial codelab release

---
+++ exoplayer-codelab-03/player-lib/src/main/java/com/example/exoplayer/PlayerActivity.java
@@ -0,0 +1,143 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+* limitations under the License.
+ */
+package com.example.exoplayer;
+
+import android.annotation.SuppressLint;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.dash.DashChunkSource;
+import com.google.android.exoplayer2.source.dash.DashMediaSource;
+import com.google.android.exoplayer2.source.dash.DefaultDashChunkSource;
+import com.google.android.exoplayer2.trackselection.AdaptiveVideoTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
+import com.google.android.exoplayer2.upstream.HttpDataSource;
+import com.google.android.exoplayer2.util.Util;
+
+/**
+ * A fullscreen activity to play audio or video streams.
+ */
+public class PlayerActivity extends AppCompatActivity {
+
+  // bandwidth meter to measure and estimate bandwidth
+  private static final DefaultBandwidthMeter BANDWIDTH_METER = new DefaultBandwidthMeter();
+
+  private SimpleExoPlayer player;
+  private SimpleExoPlayerView playerView;
+
+  private long playbackPosition;
+  private int currentWindow;
+  private boolean playWhenReady = true;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_player);
+
+    playerView = (SimpleExoPlayerView) findViewById(R.id.video_view);
+  }
+
+  @Override
+  public void onStart() {
+    super.onStart();
+    if (Util.SDK_INT > 23) {
+      initializePlayer();
+    }
+  }
+
+  @Override
+  public void onResume() {
+    super.onResume();
+    hideSystemUi();
+    if ((Util.SDK_INT <= 23 || player == null)) {
+      initializePlayer();
+    }
+  }
+
+  @Override
+  public void onPause() {
+    super.onPause();
+    if (Util.SDK_INT <= 23) {
+      releasePlayer();
+    }
+  }
+
+  @Override
+  public void onStop() {
+    super.onStop();
+    if (Util.SDK_INT > 23) {
+      releasePlayer();
+    }
+  }
+
+  private void initializePlayer() {
+    if (player == null) {
+      // a factory to create an AdaptiveVideoTrackSelection
+      TrackSelection.Factory adaptiveVideoTrackSelectionFactory =
+          new AdaptiveVideoTrackSelection.Factory(BANDWIDTH_METER);
+      // using a DefaultTrackSelector with an adaptive video selection factory
+      player = ExoPlayerFactory.newSimpleInstance(this, new DefaultTrackSelector(
+          adaptiveVideoTrackSelectionFactory), new DefaultLoadControl());
+      playerView.setPlayer(player);
+      player.setPlayWhenReady(playWhenReady);
+      player.seekTo(currentWindow, playbackPosition);
+    }
+    MediaSource mediaSource = buildMediaSource(Uri.parse(getString(R.string.media_url_dash)));
+    player.prepare(mediaSource, true, false);
+  }
+
+  private void releasePlayer() {
+    if (player != null) {
+      playbackPosition = player.getCurrentPosition();
+      currentWindow = player.getCurrentWindowIndex();
+      playWhenReady = player.getPlayWhenReady();
+      player.release();
+      player = null;
+    }
+  }
+
+  private MediaSource buildMediaSource(Uri uri) {
+    DataSource.Factory dataSourceFactory = new DefaultHttpDataSourceFactory("ua", BANDWIDTH_METER);
+    DashChunkSource.Factory dashChunkSourceFactory = new DefaultDashChunkSource.Factory(
+        dataSourceFactory);
+    return new DashMediaSource(uri, dataSourceFactory, dashChunkSourceFactory, null, null);
+  }
+
+  @SuppressLint("InlinedApi")
+  private void hideSystemUi() {
+    playerView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+        | View.SYSTEM_UI_FLAG_FULLSCREEN
+        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+  }
+
+}

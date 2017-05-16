3b422e26e22a53b5ae933be14715718c63a9577f
Marc Bächinger <bachinger@google.com>
2017-05-16 13:47:18 -0700

    final version

---
+++ exoplayer-codelab-01/player-lib/src/main/java/com/example/exoplayer/PlayerActivity.java
@@ -0,0 +1,126 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+* limitations under the License.
+ */
+package com.example.exoplayer;
+
+import android.annotation.SuppressLint;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.DefaultRenderersFactory;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
+
+/**
+ * A fullscreen activity to play audio or video streams.
+ */
+public class PlayerActivity extends AppCompatActivity {
+
+  private SimpleExoPlayer player;
+  private SimpleExoPlayerView playerView;
+
+  private long playbackPosition;
+  private int currentWindow;
+  private boolean playWhenReady = true;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_player);
+
+    playerView = (SimpleExoPlayerView) findViewById(R.id.video_view);
+  }
+
+  @Override
+  public void onStart() {
+    super.onStart();
+    if (Util.SDK_INT > 23) {
+      initializePlayer();
+    }
+  }
+
+  @Override
+  public void onResume() {
+    super.onResume();
+    hideSystemUi();
+    if ((Util.SDK_INT <= 23 || player == null)) {
+      initializePlayer();
+    }
+  }
+
+  @Override
+  public void onPause() {
+    super.onPause();
+    if (Util.SDK_INT <= 23) {
+      releasePlayer();
+    }
+  }
+
+  @Override
+  public void onStop() {
+    super.onStop();
+    if (Util.SDK_INT > 23) {
+      releasePlayer();
+    }
+  }
+
+  private void initializePlayer() {
+    if (player == null) {
+      player = ExoPlayerFactory.newSimpleInstance(new DefaultRenderersFactory(this),
+          new DefaultTrackSelector(), new DefaultLoadControl());
+      playerView.setPlayer(player);
+      player.setPlayWhenReady(playWhenReady);
+      player.seekTo(currentWindow, playbackPosition);
+    }
+    MediaSource mediaSource = buildMediaSource(Uri.parse(getString(R.string.media_url_mp4)));
+    player.prepare(mediaSource, true, false);
+  }
+
+  private void releasePlayer() {
+    if (player != null) {
+      playbackPosition = player.getCurrentPosition();
+      currentWindow = player.getCurrentWindowIndex();
+      playWhenReady = player.getPlayWhenReady();
+      player.release();
+      player = null;
+    }
+  }
+
+  private MediaSource buildMediaSource(Uri uri) {
+    return new ExtractorMediaSource(uri, new DefaultHttpDataSourceFactory("exoplayer-codelab"),
+        new DefaultExtractorsFactory(), null, null);
+  }
+
+  @SuppressLint("InlinedApi")
+  private void hideSystemUi() {
+    playerView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+        | View.SYSTEM_UI_FLAG_FULLSCREEN
+        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+  }
+
+}
---
+++ exoplayer-codelab-04/player-lib/src/main/java/com/example/exoplayer/PlayerActivity.java
@@ -0,0 +1,283 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+* limitations under the License.
+ */
+package com.example.exoplayer;
+
+import android.annotation.SuppressLint;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Surface;
+import android.view.View;
+
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.DefaultRenderersFactory;
+import com.google.android.exoplayer2.ExoPlaybackException;
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.Format;
+import com.google.android.exoplayer2.PlaybackParameters;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.Timeline;
+import com.google.android.exoplayer2.audio.AudioRendererEventListener;
+import com.google.android.exoplayer2.decoder.DecoderCounters;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.TrackGroupArray;
+import com.google.android.exoplayer2.source.dash.DashChunkSource;
+import com.google.android.exoplayer2.source.dash.DashMediaSource;
+import com.google.android.exoplayer2.source.dash.DefaultDashChunkSource;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
+import com.google.android.exoplayer2.video.VideoRendererEventListener;
+
+/**
+ * A fullscreen activity to play audio or video streams.
+ */
+public class PlayerActivity extends AppCompatActivity {
+
+  // bandwidth meter to measure and estimate bandwidth
+  private static final DefaultBandwidthMeter BANDWIDTH_METER = new DefaultBandwidthMeter();
+  private static final String TAG = "PlayerActivity";
+
+  private SimpleExoPlayer player;
+  private SimpleExoPlayerView playerView;
+  private ComponentListener componentListener;
+
+  private long playbackPosition;
+  private int currentWindow;
+  private boolean playWhenReady = true;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_player);
+
+    componentListener = new ComponentListener();
+    playerView = (SimpleExoPlayerView) findViewById(R.id.video_view);
+  }
+
+  @Override
+  public void onStart() {
+    super.onStart();
+    if (Util.SDK_INT > 23) {
+      initializePlayer();
+    }
+  }
+
+  @Override
+  public void onResume() {
+    super.onResume();
+    hideSystemUi();
+    if ((Util.SDK_INT <= 23 || player == null)) {
+      initializePlayer();
+    }
+  }
+
+  @Override
+  public void onPause() {
+    super.onPause();
+    if (Util.SDK_INT <= 23) {
+      releasePlayer();
+    }
+  }
+
+  @Override
+  public void onStop() {
+    super.onStop();
+    if (Util.SDK_INT > 23) {
+      releasePlayer();
+    }
+  }
+
+  private void initializePlayer() {
+    if (player == null) {
+      // a factory to create an AdaptiveVideoTrackSelection
+      TrackSelection.Factory adaptiveTrackSelectionFactory =
+          new AdaptiveTrackSelection.Factory(BANDWIDTH_METER);
+      // using a DefaultTrackSelector with an adaptive video selection factory
+      player = ExoPlayerFactory.newSimpleInstance(new DefaultRenderersFactory(this),
+          new DefaultTrackSelector(adaptiveTrackSelectionFactory), new DefaultLoadControl());
+      player.addListener(componentListener);
+      player.setVideoDebugListener(componentListener);
+      player.setAudioDebugListener(componentListener);
+      playerView.setPlayer(player);
+      player.setPlayWhenReady(playWhenReady);
+      player.seekTo(currentWindow, playbackPosition);
+    }
+    MediaSource mediaSource = buildMediaSource(Uri.parse(getString(R.string.media_url_dash)));
+    player.prepare(mediaSource, true, false);
+  }
+
+  private void releasePlayer() {
+    if (player != null) {
+      playbackPosition = player.getCurrentPosition();
+      currentWindow = player.getCurrentWindowIndex();
+      playWhenReady = player.getPlayWhenReady();
+      player.removeListener(componentListener);
+      player.setVideoListener(null);
+      player.setVideoDebugListener(null);
+      player.setAudioDebugListener(null);
+      player.release();
+      player = null;
+    }
+  }
+
+  private MediaSource buildMediaSource(Uri uri) {
+    DataSource.Factory dataSourceFactory = new DefaultHttpDataSourceFactory("ua", BANDWIDTH_METER);
+    DashChunkSource.Factory dashChunkSourceFactory = new DefaultDashChunkSource.Factory(
+        dataSourceFactory);
+    return new DashMediaSource(uri, dataSourceFactory, dashChunkSourceFactory, null, null);
+  }
+
+  @SuppressLint("InlinedApi")
+  private void hideSystemUi() {
+    playerView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+        | View.SYSTEM_UI_FLAG_FULLSCREEN
+        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+  }
+
+  private class ComponentListener implements ExoPlayer.EventListener, VideoRendererEventListener,
+      AudioRendererEventListener {
+    @Override
+    public void onTimelineChanged(Timeline timeline, Object manifest) {
+      // Do nothing.
+    }
+
+    @Override
+    public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {
+      // Do nothing.
+    }
+
+    @Override
+    public void onLoadingChanged(boolean isLoading) {
+      // Do nothing.
+    }
+
+    @Override
+    public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+      String stateString;
+      switch (playbackState) {
+        case ExoPlayer.STATE_IDLE:
+          stateString = "ExoPlayer.STATE_IDLE      -";
+          break;
+        case ExoPlayer.STATE_BUFFERING:
+          stateString = "ExoPlayer.STATE_BUFFERING -";
+          break;
+        case ExoPlayer.STATE_READY:
+          stateString = "ExoPlayer.STATE_READY     -";
+          break;
+        case ExoPlayer.STATE_ENDED:
+          stateString = "ExoPlayer.STATE_ENDED     -";
+          break;
+        default:
+          stateString = "UNKNOWN_STATE             -";
+          break;
+      }
+      Log.d(TAG, "changed state to " + stateString + " playWhenReady: " + playWhenReady);
+    }
+
+    @Override
+    public void onPlayerError(ExoPlaybackException error) {
+      // Do nothing.
+    }
+
+    @Override
+    public void onPositionDiscontinuity() {
+      // Do nothing.
+    }
+
+    @Override
+    public void onPlaybackParametersChanged(PlaybackParameters playbackParameters) {
+      // Do nothing.
+    }
+
+    @Override
+    public void onVideoEnabled(DecoderCounters counters) {
+
+    }
+
+    @Override
+    public void onVideoDecoderInitialized(String decoderName, long initializedTimestampMs, long initializationDurationMs) {
+
+    }
+
+    @Override
+    public void onVideoInputFormatChanged(Format format) {
+
+    }
+
+    @Override
+    public void onDroppedFrames(int count, long elapsedMs) {
+
+    }
+
+    @Override
+    public void onVideoSizeChanged(int width, int height, int unappliedRotationDegrees, float pixelWidthHeightRatio) {
+
+    }
+
+    @Override
+    public void onRenderedFirstFrame(Surface surface) {
+
+    }
+
+    @Override
+    public void onVideoDisabled(DecoderCounters counters) {
+
+    }
+
+    @Override
+    public void onAudioEnabled(DecoderCounters counters) {
+
+    }
+
+    @Override
+    public void onAudioSessionId(int audioSessionId) {
+
+    }
+
+    @Override
+    public void onAudioDecoderInitialized(String decoderName, long initializedTimestampMs, long initializationDurationMs) {
+
+    }
+
+    @Override
+    public void onAudioInputFormatChanged(Format format) {
+
+    }
+
+    @Override
+    public void onAudioTrackUnderrun(int bufferSize, long bufferSizeMs, long elapsedSinceLastFeedMs) {
+
+    }
+
+    @Override
+    public void onAudioDisabled(DecoderCounters counters) {
+
+    }
+  }
+
+}

8b538528dff38f8325462e185c4c3c14cd3632e1
Don Turner <donturner@google.com>
2019-10-11 09:26:56 +0100

    Remove code which should be added during codelab

--- exoplayer-codelab-03/player-lib/src/main/java/com/example/exoplayer/PlayerActivity.java
+++ exoplayer-codelab-03/player-lib/src/main/java/com/example/exoplayer/PlayerActivity.java
@@ -11,7 +11,7 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
-* limitations under the License.
+ * limitations under the License.
  */
 package com.example.exoplayer;
 
@@ -19,18 +19,12 @@ import android.annotation.SuppressLint;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.Surface;
 import android.view.View;
 
 import com.google.android.exoplayer2.DefaultLoadControl;
 import com.google.android.exoplayer2.DefaultRenderersFactory;
 import com.google.android.exoplayer2.ExoPlayerFactory;
-import com.google.android.exoplayer2.Format;
-import com.google.android.exoplayer2.Player;
 import com.google.android.exoplayer2.SimpleExoPlayer;
-import com.google.android.exoplayer2.audio.AudioRendererEventListener;
-import com.google.android.exoplayer2.decoder.DecoderCounters;
 import com.google.android.exoplayer2.source.MediaSource;
 import com.google.android.exoplayer2.source.dash.DashChunkSource;
 import com.google.android.exoplayer2.source.dash.DashMediaSource;
@@ -43,20 +37,17 @@ import com.google.android.exoplayer2.upstream.DataSource;
 import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
 import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
 import com.google.android.exoplayer2.util.Util;
-import com.google.android.exoplayer2.video.VideoRendererEventListener;
 
 /**
  * A fullscreen activity to play audio or video streams.
  */
 public class PlayerActivity extends AppCompatActivity {
 
-  // bandwidth meter to measure and estimate bandwidth
-  private static final DefaultBandwidthMeter BANDWIDTH_METER = new DefaultBandwidthMeter();
-  private static final String TAG = "PlayerActivity";
+  private static final DefaultBandwidthMeter BANDWIDTH_METER =
+          new DefaultBandwidthMeter();
 
   private SimpleExoPlayer player;
   private PlayerView playerView;
-  private ComponentListener componentListener;
 
   private long playbackPosition;
   private int currentWindow;
@@ -67,7 +58,6 @@ public class PlayerActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_player);
 
-    componentListener = new ComponentListener();
     playerView = findViewById(R.id.video_view);
   }
 
@@ -108,13 +98,13 @@ public class PlayerActivity extends AppCompatActivity {
     if (player == null) {
       // a factory to create an AdaptiveVideoTrackSelection
       TrackSelection.Factory adaptiveTrackSelectionFactory =
-          new AdaptiveTrackSelection.Factory(BANDWIDTH_METER);
-      // using a DefaultTrackSelector with an adaptive video selection factory
-      player = ExoPlayerFactory.newSimpleInstance(new DefaultRenderersFactory(this),
-          new DefaultTrackSelector(adaptiveTrackSelectionFactory), new DefaultLoadControl());
-      player.addListener(componentListener);
-      player.addVideoDebugListener(componentListener);
-      player.addAudioDebugListener(componentListener);
+              new AdaptiveTrackSelection.Factory(BANDWIDTH_METER);
+
+      player = ExoPlayerFactory.newSimpleInstance(
+              new DefaultRenderersFactory(this),
+              new DefaultTrackSelector(adaptiveTrackSelectionFactory),
+              new DefaultLoadControl());
+
       playerView.setPlayer(player);
       player.setPlayWhenReady(playWhenReady);
       player.seekTo(currentWindow, playbackPosition);
@@ -128,9 +118,6 @@ public class PlayerActivity extends AppCompatActivity {
       playbackPosition = player.getCurrentPosition();
       currentWindow = player.getCurrentWindowIndex();
       playWhenReady = player.getPlayWhenReady();
-      player.removeListener(componentListener);
-      player.removeVideoDebugListener(componentListener);
-      player.removeAudioDebugListener(componentListener);
       player.release();
       player = null;
     }
@@ -153,103 +140,6 @@ public class PlayerActivity extends AppCompatActivity {
         | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
         | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
         | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-  }
-
-  private class ComponentListener extends Player.DefaultEventListener implements
-      VideoRendererEventListener, AudioRendererEventListener {
-
-    @Override
-    public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
-      String stateString;
-      switch (playbackState) {
-        case Player.STATE_IDLE:
-          stateString = "ExoPlayer.STATE_IDLE      -";
-          break;
-        case Player.STATE_BUFFERING:
-          stateString = "ExoPlayer.STATE_BUFFERING -";
-          break;
-        case Player.STATE_READY:
-          stateString = "ExoPlayer.STATE_READY     -";
-          break;
-        case Player.STATE_ENDED:
-          stateString = "ExoPlayer.STATE_ENDED     -";
-          break;
-        default:
-          stateString = "UNKNOWN_STATE             -";
-          break;
-      }
-      Log.d(TAG, "changed state to " + stateString + " playWhenReady: " + playWhenReady);
-    }
-
-    // Implementing VideoRendererEventListener.
-
-    @Override
-    public void onVideoEnabled(DecoderCounters counters) {
-      // Do nothing.
-    }
-
-    @Override
-    public void onVideoDecoderInitialized(String decoderName, long initializedTimestampMs, long initializationDurationMs) {
-      // Do nothing.
-    }
-
-    @Override
-    public void onVideoInputFormatChanged(Format format) {
-      // Do nothing.
-    }
-
-    @Override
-    public void onDroppedFrames(int count, long elapsedMs) {
-      // Do nothing.
-    }
-
-    @Override
-    public void onVideoSizeChanged(int width, int height, int unappliedRotationDegrees, float pixelWidthHeightRatio) {
-      // Do nothing.
-    }
-
-    @Override
-    public void onRenderedFirstFrame(Surface surface) {
-      // Do nothing.
-    }
-
-    @Override
-    public void onVideoDisabled(DecoderCounters counters) {
-      // Do nothing.
-    }
-
-    // Implementing AudioRendererEventListener.
-
-    @Override
-    public void onAudioEnabled(DecoderCounters counters) {
-      // Do nothing.
-    }
-
-    @Override
-    public void onAudioSessionId(int audioSessionId) {
-      // Do nothing.
-    }
-
-    @Override
-    public void onAudioDecoderInitialized(String decoderName, long initializedTimestampMs, long initializationDurationMs) {
-      // Do nothing.
-    }
-
-    @Override
-    public void onAudioInputFormatChanged(Format format) {
-      // Do nothing.
-    }
-
-    @Override
-    public void onAudioSinkUnderrun(int bufferSize, long bufferSizeMs, long elapsedSinceLastFeedMs) {
-      // Do nothing.
-    }
-
-    @Override
-    public void onAudioDisabled(DecoderCounters counters) {
-      // Do nothing.
-    }
-
   }
 
 }

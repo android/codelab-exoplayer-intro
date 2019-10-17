5b50b70e4f0f99bfe6562575919b5c9928232f72
Don Turner <donturner@google.com>
2019-10-17 15:40:33 +0100

    Save state of codelab-04

--- exoplayer-codelab-04/player-lib/src/main/java/com/example/exoplayer/PlayerActivity.java
+++ exoplayer-codelab-04/player-lib/src/main/java/com/example/exoplayer/PlayerActivity.java
@@ -11,64 +11,54 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
-* limitations under the License.
+ * limitations under the License.
  */
 package com.example.exoplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.Surface;
 import android.view.View;
 
-import com.google.android.exoplayer2.DefaultLoadControl;
-import com.google.android.exoplayer2.DefaultRenderersFactory;
+import com.google.android.exoplayer2.ExoPlayer;
 import com.google.android.exoplayer2.ExoPlayerFactory;
-import com.google.android.exoplayer2.Format;
 import com.google.android.exoplayer2.Player;
 import com.google.android.exoplayer2.SimpleExoPlayer;
-import com.google.android.exoplayer2.audio.AudioRendererEventListener;
-import com.google.android.exoplayer2.decoder.DecoderCounters;
 import com.google.android.exoplayer2.source.MediaSource;
-import com.google.android.exoplayer2.source.dash.DashChunkSource;
 import com.google.android.exoplayer2.source.dash.DashMediaSource;
-import com.google.android.exoplayer2.source.dash.DefaultDashChunkSource;
 import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
 import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
 import com.google.android.exoplayer2.ui.PlayerView;
-import com.google.android.exoplayer2.upstream.DataSource;
-import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
 import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
 import com.google.android.exoplayer2.util.Util;
-import com.google.android.exoplayer2.video.VideoRendererEventListener;
 
 /**
  * A fullscreen activity to play audio or video streams.
  */
 public class PlayerActivity extends AppCompatActivity {
 
-  // bandwidth meter to measure and estimate bandwidth
-  private static final DefaultBandwidthMeter BANDWIDTH_METER = new DefaultBandwidthMeter();
+  private PlaybackStateListener playbackStateListener;
   private static final String TAG = PlayerActivity.class.getName();
 
-  private SimpleExoPlayer player;
   private PlayerView playerView;
-  private ComponentListener componentListener;
-
-  private long playbackPosition;
-  private int currentWindow;
+  private SimpleExoPlayer player;
   private boolean playWhenReady = true;
+  private int currentWindow = 0;
+  private long playbackPosition = 0;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_player);
 
-    componentListener = new ComponentListener();
     playerView = findViewById(R.id.video_view);
+
+    playbackStateListener = new PlaybackStateListener();
   }
 
   @Override
@@ -106,21 +96,20 @@ public class PlayerActivity extends AppCompatActivity {
 
   private void initializePlayer() {
     if (player == null) {
-      // a factory to create an AdaptiveVideoTrackSelection
       TrackSelection.Factory adaptiveTrackSelectionFactory =
-          new AdaptiveTrackSelection.Factory(BANDWIDTH_METER);
-      // using a DefaultTrackSelector with an adaptive video selection factory
-      player = ExoPlayerFactory.newSimpleInstance(new DefaultRenderersFactory(this),
-          new DefaultTrackSelector(adaptiveTrackSelectionFactory), new DefaultLoadControl());
-      player.addListener(componentListener);
-      player.addVideoDebugListener(componentListener);
-      player.addAudioDebugListener(componentListener);
-      playerView.setPlayer(player);
-      player.setPlayWhenReady(playWhenReady);
-      player.seekTo(currentWindow, playbackPosition);
+              new AdaptiveTrackSelection.Factory();
+      TrackSelector trackSelector = new DefaultTrackSelector(adaptiveTrackSelectionFactory);
+      player = ExoPlayerFactory.newSimpleInstance(this, trackSelector);
     }
-    MediaSource mediaSource = buildMediaSource(Uri.parse(getString(R.string.media_url_dash)));
-    player.prepare(mediaSource, true, false);
+
+    playerView.setPlayer(player);
+    Uri uri = Uri.parse(getString(R.string.media_url_dash));
+    MediaSource mediaSource = buildMediaSource(uri);
+
+    player.setPlayWhenReady(playWhenReady);
+    player.seekTo(currentWindow, playbackPosition);
+    player.addListener(playbackStateListener);
+    player.prepare(mediaSource, false, false);
   }
 
   private void releasePlayer() {
@@ -128,127 +117,55 @@ public class PlayerActivity extends AppCompatActivity {
       playbackPosition = player.getCurrentPosition();
       currentWindow = player.getCurrentWindowIndex();
       playWhenReady = player.getPlayWhenReady();
-      player.removeListener(componentListener);
-      player.removeVideoDebugListener(componentListener);
-      player.removeAudioDebugListener(componentListener);
+      player.removeListener(playbackStateListener);
       player.release();
       player = null;
     }
   }
 
   private MediaSource buildMediaSource(Uri uri) {
-    DataSource.Factory manifestDataSourceFactory = new DefaultHttpDataSourceFactory("exoplayer-codelab");
-    DashChunkSource.Factory dashChunkSourceFactory = new DefaultDashChunkSource.Factory(
-        new DefaultHttpDataSourceFactory("exoplayer-codelab", BANDWIDTH_METER));
-    return new DashMediaSource.Factory(dashChunkSourceFactory, manifestDataSourceFactory)
-        .createMediaSource(uri);
+    DefaultHttpDataSourceFactory httpDataSourceFactory =
+            new DefaultHttpDataSourceFactory("exoplayer-codelab");
+    DashMediaSource.Factory mediaSourceFactory = new DashMediaSource.Factory(httpDataSourceFactory);
+    return mediaSourceFactory.createMediaSource(uri);
   }
 
   @SuppressLint("InlinedApi")
   private void hideSystemUi() {
     playerView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-        | View.SYSTEM_UI_FLAG_FULLSCREEN
-        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+            | View.SYSTEM_UI_FLAG_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
   }
 
-  private class ComponentListener extends Player.DefaultEventListener implements
-      VideoRendererEventListener, AudioRendererEventListener {
+  private class PlaybackStateListener implements Player.EventListener{
 
     @Override
-    public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+    public void onPlayerStateChanged(boolean playWhenReady,
+                                     int playbackState) {
       String stateString;
       switch (playbackState) {
-        case Player.STATE_IDLE:
+        case ExoPlayer.STATE_IDLE:
           stateString = "ExoPlayer.STATE_IDLE      -";
           break;
-        case Player.STATE_BUFFERING:
+        case ExoPlayer.STATE_BUFFERING:
           stateString = "ExoPlayer.STATE_BUFFERING -";
           break;
-        case Player.STATE_READY:
+        case ExoPlayer.STATE_READY:
           stateString = "ExoPlayer.STATE_READY     -";
           break;
-        case Player.STATE_ENDED:
+        case ExoPlayer.STATE_ENDED:
           stateString = "ExoPlayer.STATE_ENDED     -";
           break;
         default:
           stateString = "UNKNOWN_STATE             -";
           break;
       }
-      Log.d(TAG, "changed state to " + stateString + " playWhenReady: " + playWhenReady);
+      Log.d(TAG, "changed state to " + stateString
+              + " playWhenReady: " + playWhenReady);
     }
-
-    // Implementing VideoRendererEventListener.
-
-    @Override
-    public void onVideoEnabled(DecoderCounters counters) {
-      // Do nothing.
-    }
-
-    @Override
-    public void onVideoDecoderInitialized(String decoderName, long initializedTimestampMs, long initializationDurationMs) {
-      // Do nothing.
-    }
-
-    @Override
-    public void onVideoInputFormatChanged(Format format) {
-      // Do nothing.
-    }
-
-    @Override
-    public void onDroppedFrames(int count, long elapsedMs) {
-      // Do nothing.
-    }
-
-    @Override
-    public void onVideoSizeChanged(int width, int height, int unappliedRotationDegrees, float pixelWidthHeightRatio) {
-      // Do nothing.
-    }
-
-    @Override
-    public void onRenderedFirstFrame(Surface surface) {
-      // Do nothing.
-    }
-
-    @Override
-    public void onVideoDisabled(DecoderCounters counters) {
-      // Do nothing.
-    }
-
-    // Implementing AudioRendererEventListener.
-
-    @Override
-    public void onAudioEnabled(DecoderCounters counters) {
-      // Do nothing.
-    }
-
-    @Override
-    public void onAudioSessionId(int audioSessionId) {
-      // Do nothing.
-    }
-
-    @Override
-    public void onAudioDecoderInitialized(String decoderName, long initializedTimestampMs, long initializationDurationMs) {
-      // Do nothing.
-    }
-
-    @Override
-    public void onAudioInputFormatChanged(Format format) {
-      // Do nothing.
-    }
-
-    @Override
-    public void onAudioSinkUnderrun(int bufferSize, long bufferSizeMs, long elapsedSinceLastFeedMs) {
-      // Do nothing.
-    }
-
-    @Override
-    public void onAudioDisabled(DecoderCounters counters) {
-      // Do nothing.
-    }
-
   }
 
 }
